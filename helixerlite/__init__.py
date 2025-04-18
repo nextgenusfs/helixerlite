d41fa2d7459922b01d41e0e1512e34b425f06872
Jon Palmer <nextgenusfs@gmail.com>
2025-04-18 09:05:32 -0700

    Revert "Simplify maturin configuration and update __init__.py to properly handle helixerpost import"
    This reverts commit 5549839bf830a884d09e86fbf222dcfd1f2482a4.

--- helixerlite/__init__.py
+++ helixerlite/__init__.py
@@ -1,14 +1,11 @@
 import importlib.metadata
 import sys
 
-# Re-export helixerpost functions at the helixerlite level
+# Make helixerpost available at the package level
 try:
     import helixerpost
 
-    # Make helixerpost available as helixerlite.helixerpost
     sys.modules["helixerlite.helixerpost"] = helixerpost
-    # Re-export helixerpost functions
-    from helixerpost import *
 except ImportError as e:
     print(f"Error importing helixerpost module: {e}")
 
